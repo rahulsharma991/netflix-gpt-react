00e00095147a833e2d103a9e5f2034c583ae3e9f
Rahul Sharma <rahulsharma@beatroute.io>
2024-02-25 20:57:07 +0530

    gpt search

---
+++ src/components/GPTsearch.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GPTsearch = () => {
+  return (
+    <div>
+      <p>GPT search</p>
+    </div>
+  )
+}
+
+export default GPTsearch
--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -3,7 +3,6 @@ import MovieList from './MovieList'
 import {useSelector} from 'react-redux';
 const SecondaryContainer = () => {
   const movies = useSelector(store => store.movies);
-  console.log(movies)
   if(!movies?.nowPlayingMovies) return;
   return (
     <div className='bg-black'>
